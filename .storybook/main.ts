1fd03504bb5243b9c509e72d6160b240bcfad6d9
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-10-13 03:00:16 -0300

    initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/components/**/*.stories.tsx"],
+  staticDirs: ["../src/assets"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@storybook/addon-designs",
+    "@storybook/addon-a11y",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+export default config;
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import styles from "./App.module.sass";
+
+function App() {
+  return <div className={styles.app}>Hello World</div>;
+}
+
+export default App;
